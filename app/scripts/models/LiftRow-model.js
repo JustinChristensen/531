4936a8627f09d9e94fa097f5da5884e46c5c5c14
Justin Christensen <wroathe@gmail.com>
2012-12-09 15:56:50 -0600

    Added Lift Row model

---
+++ app/scripts/models/LiftRow-model.js
@@ -0,0 +1,15 @@
+(function (window, $, undefined) {
+  'use strict';
+
+  _531.Models.LiftRowModel = Backbone.Model.extend({
+
+    defaults: {
+      lift: ''
+    },
+
+    initialize: function (attributes) {
+    }
+  
+  });
+
+}(window, jQuery));
