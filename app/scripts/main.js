55e9dc3f9ffbf9f91ca5341bd62554944dbfe363
Justin Christensen <wroathe@gmail.com>
2012-12-09 16:03:53 -0600

    Updated main script

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,44 @@
+/**
+ * Initialize calculator global object
+ * 531 Calculator is a Backbone Application
+ *
+ * Justin Christensen
+ * Sun Dec  9 15:59:09 2012
+ */
+(function (window, $, undefined) {
+  'use strict';
 
-window.531 = {
-  Models: {},
-  Collections: {},
-  Views: {},
-  Routers: {},
-  init: function() {
-    console.log('Hello from Backbone!');
-  }
-};
+  window._531 = {
+    /**
+     * Backbone Objects
+     */
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
 
-$(document).ready(function(){
-  531.init();
-});
+    /**
+     * General Utilities
+     */
+    utils: {
+      keys: {
+        ENTER: 13
+      }
+    },
+
+    /**
+     * Main Driver
+     */
+    init: function() {
+    }
+
+  };
+
+  // Execute application after document is ready
+  // TODO: Determine if this is necessary
+  $(document).ready(function(){
+    _531.init();
+  });
+
+}(window, jQuery));
+
