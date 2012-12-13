fbc85d981528e62f89fce80e04e403d60dda0348
Justin Christensen <wroathe@gmail.com>
2012-12-12 21:23:03 -0600

    Updated styles and package.json

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,7 +10,7 @@
 
   window._531 = {
     /**
-     * Backbone Objects
+     * Backbone Namespaces
      */
     Models: {},
     Collections: {},
@@ -23,13 +23,54 @@
     utils: {
       keys: {
         ENTER: 13
+      },
+
+      toPercent: function(num) {
+        return num >= 1 ? num / 100 : num;
       }
     },
+
+    /**
+     * Reps and Percentages for Cell View
+     */
+    program: {
+      percentages: {
+        0: [65, 75, 85],
+        1: [70, 80, 90],
+        2: [75, 85, 95],
+        3: [40, 50, 60]
+      },
+
+      reps: {
+        0: ['5', '5', '5'],
+        1: ['3', '3', '3'],
+        2: ['5', '3', '1'],
+        3: ['5', '5', '5']
+      }
+    },
+
+    /**
+     * On first load
+     */
+    initialRows: [
+      { lift: 'Press', order: 0 },
+      { lift: 'Deadlift', order: 1 },
+      { lift: 'Bench', order: 2 },
+      { lift: 'Squat', order: 3 }
+    ],
 
     /**
      * Main Driver
      */
     init: function() {
+      var ApplicationView = this.Views.ApplicationView,
+        LiftRows = this.Collections.LiftRows;
+
+      // Create instance to bind handlers to
+      this.rows = new LiftRows();
+
+      // Fire things off
+      new ApplicationView();
     }
 
   };
@@ -41,4 +82,3 @@
   });
 
 }(window, jQuery));
-
